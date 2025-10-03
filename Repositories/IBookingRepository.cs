d895fc9f305f5b4613607925a8d63e8c80ad4731
Waithwhat <135276403+Waithwhat@users.noreply.github.com>
2025-10-03 20:59:51 +0500

    DTOS, Services, Data

--- Repositories/IBookingRepository.cs
+++
@@ -1,9 +0,0 @@
-using RestaurantBooking.Models;
-
-namespace RestaurantBooking.Repositories;
-
-public interface IBookingRepository : IRepository<Booking>
-{
-    Task<IEnumerable<Booking>> GetBookingsForTable(Guid tableId, DateTime from, DateTime to);
-    Task<IEnumerable<Booking>> GetBookingsForUser(Guid userId);
-}
